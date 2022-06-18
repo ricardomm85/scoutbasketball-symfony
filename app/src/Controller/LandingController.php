448ef1c25098a4579f736b245d3f9f393a577640
ricardo <ricardo@scoutbasketball.com>
2022-06-18 18:12:15 +0200

    initial

---
+++ app/src/Controller/LandingController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LandingController
+{
+    public function __invoke(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html lang="en"><body>Hello world! '.$number.'</body></html>'
+        );
+    }
+}
